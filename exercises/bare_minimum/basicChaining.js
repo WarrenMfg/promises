e7434889fe435003b3bea7525729363473ecf32c
WarrenMfg <warrenmfg@icloud.com>
2020-01-07 17:00:13 -0500

    completed bare-minimum

--- exercises/bare_minimum/basicChaining.js
+++ exercises/bare_minimum/basicChaining.js
@@ -11,27 +11,37 @@
 var fs = require('fs');
 var Promise = require('bluebird');
 var request = require('request');
-
+let getGitHubProfileAsync = require('./promisification').getGitHubProfileAsync;
+let writeFileAsync = Promise.promisify(fs.writeFile);
 
 
 var fetchProfileAndWriteToFile = function(readFilePath, writeFilePath) {
   // TODO
-  return new Promise((resolve, reject) => {
-    // read file here, pass along contents to then
-    // assemble url
-    'https://api.github.com/users/' + fileContent // add search/???????
+  let pluckFirstLineFromFileAsync = function(filePath) {
+    // TODO
+    return new Promise((resolve, reject) => {
+      fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+          reject(err);
+        } else {
+          let dataArr = data.split('\n');
+          resolve(dataArr[0]);
+        }
+      });
 
-  }).then(url => {
-    request(url, (err, res, body) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(res);
-      }
+    }).then(firstLine => {
+      return firstLine;
+      })
+      .catch(err => {
+        throw err;
+      })
+  };
+
+ return pluckFirstLineFromFileAsync(readFilePath)
+    .then(getGitHubProfileAsync)
+    .then((data) => {
+      return writeFileAsync(writeFilePath, JSON.stringify(data));
     });
-  }).then(data => {
-    // write to file path
-  }).catch(err => {throw err});
 };
 
 // Export these functions so we can test them
@@ -39,23 +49,50 @@ module.exports = {
   fetchProfileAndWriteToFile: fetchProfileAndWriteToFile
 };
 
+
+// return new Promise((resolve, reject) => {
+//   // read file here, pass along contents to then
+//   fs.readFile(readFilePath, 'utf8', (err, data) => {
+//     if (err) {
+//       reject(err);
+//     } else {
+//       let dataArr = data.split('\n');
+//       resolve(`https://api.github.com/users/${dataArr[0]}`);
+//     }
+//   });
+
+// }).then(url => {
+
+//   request(url, (err, res, body) => {
+//     if (err) {
+//       return err;
+//     } else {
+//       return body;
+//     }
+//   });
+
+// }).then(data => {
+//   // write to file path
+//   fs.writeFile(writeFilePath, data, () => {});
+
+// }).catch(err => {throw err});
 ////////////////////////////////////////////////////////////////////////////
-var getGitHubProfile = function(user, callback) {
-  var options = {
-    url: 'https://api.github.com/users/' + user,
-    headers: { 'User-Agent': 'request' },
-    json: true  // will JSON.parse(body) for us
-  };
+// var getGitHubProfile = function(user, callback) {
+//   var options = {
+//     url: 'https://api.github.com/users/' + user,
+//     headers: { 'User-Agent': 'request' },
+//     json: true  // will JSON.parse(body) for us
+//   };
 
-  request.get(options, function(err, res, body) {
-    if (err) {
-      callback(err, null);
-    } else if (body.message) {
-      callback(new Error('Failed to get GitHub profile: ' + body.message), null);
-    } else {
-      callback(null, body);
-    }
-  });
-};
+//   request.get(options, function(err, res, body) {
+//     if (err) {
+//       callback(err, null);
+//     } else if (body.message) {
+//       callback(new Error('Failed to get GitHub profile: ' + body.message), null);
+//     } else {
+//       callback(null, body);
+//     }
+//   });
+// };
 
-var getGitHubProfileAsync = Promise.promisify(getGitHubProfile); // TODO
+// var getGitHubProfileAsync = Promise.promisify(getGitHubProfile); // TODO
